a11c040cb625d8fb23530596385fe9e26adb1262
janrezkasilag <janrezv.kasilag@gmail.com>
2023-11-23 22:38:27 +0800

    Initialize

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const surveyForm = document.getElementById('surveyForm');
+  
+    surveyForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+  
+      const formData = new FormData(surveyForm);
+      const surveyData = {};
+      formData.forEach((value, key) => {
+        surveyData[key] = value;
+      });
+  
+      try {
+        const response = await fetch('/submit-survey', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(surveyData),
+        });
+  
+        if (response.ok) {
+          alert('Survey submitted successfully!');
+          surveyForm.reset();
+        } else {
+          const errorMessage = await response.text();
+          alert(`Error: ${errorMessage}`);
+        }
+      } catch (error) {
+        console.error('Error submitting survey:', error);
+        alert('An error occurred while submitting the survey. Please try again.');
+      }
+    });
+  });
+  
